dc3ace302d43ec5e56536f743c05f244088c7b98
Tan Nguyen <bivani2805@gmail.com>
2025-07-19 01:53:32 -1000

    Add more rule to rule screen

--- src/components/RuleScreen.js
+++ src/components/RuleScreen.js
@@ -19,6 +19,7 @@ export const RuleScreen = () => {
                         <li className="mt-2">Press/Enter to submit the answer</li>
                         <li className="mt-2">The minutes values are ony 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60</li>
                         <li className="mt-2">The hours values are only 1,2,3,4,5,6,7,8,9,10,12</li>
+                        <li className="mt-2">Keep an eyes out on the reward tab, it automatically claimed your reward when you reach your points</li>
                     </ul>
                 </div>
             </div>
