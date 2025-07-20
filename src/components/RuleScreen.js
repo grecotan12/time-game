97e1c3126c80345bba808c7dcfee7749d2963e7d
Tan Nguyen <bivani2805@gmail.com>
2025-07-19 23:55:46 -1000

    Update rule for more accuracy

--- src/components/RuleScreen.js
+++ src/components/RuleScreen.js
@@ -18,7 +18,7 @@ export const RuleScreen = () => {
                         <li className="mt-2">Choose your gender for type of rewards then click "Play"</li>
                         <li className="mt-2">Look at Analog Clock</li>
                         <li className="mt-2">Enter the digital time</li>
-                        <li className="mt-2">Press/Enter to submit the answer</li>
+                        <li className="mt-2">Click submit to check the answer</li>
                         <li className="mt-2">The minutes values are ony 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60</li>
                         <li className="mt-2">The hours values are only 1,2,3,4,5,6,7,8,9,10,12</li>
                         <li className="mt-2">If you get the correct answer, you will get a point, get up to 5,10,15 for your desired reward</li>
